6d6aa01cb6406a0d96f53908c4a33e2ce13b3bb3
mshap <shapirom@gmail.com>
2025-12-13 21:52:52 -0500

    day9: part1

---
+++ cmd/day9/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"aoc2025/internal/files"
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type tile struct {
+	col, row int
+}
+
+func main() {
+
+	reds := files.ProcessFile("../../internal/input/day9.txt", false, true, func(line string) (tile, error) {
+		parts := strings.Split(line, ",")
+
+		x, _ := strconv.Atoi(parts[1])
+		y, _ := strconv.Atoi(parts[0])
+		return tile{row: x, col: y}, nil
+	})
+
+	slices.SortFunc(reds, func(a, b tile) int {
+		if a.row < b.row {
+			return -1
+		} else if a.row > b.row {
+			return 1
+		} else {
+			if a.col < b.col {
+				return -1
+			} else if a.col > b.col {
+				return 1
+			} else {
+				return 0
+			}
+		}
+	})
+	// fmt.Printf("%+v\n", reds)
+	max := 0.0
+
+	for idx, tile := range reds {
+		last := tile
+		for i := 1; i < len(reds); i++ {
+			if i == idx {
+				continue
+			}
+			cur := reds[i]
+			colDist := cur.col - last.col + 1
+			rowDist := cur.row - last.row + 1
+			size := math.Abs(float64(colDist * rowDist))
+			if size > max {
+				max = size
+				// last = cur
+			}
+		}
+	}
+
+	fmt.Printf("Max size: %.0f\n", max)
+
+}
